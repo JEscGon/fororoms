0d1615056fee0042b0c51fd3b24e825967f037a9
Jesús Escolar González <JesusE.G@outlook.es>
2024-11-20 16:34:35 +0100

    pagina de error personalizada

---
+++ gateway/src/main/java/com/fororoms/gateway/controller/CustomErrorController.java
@@ -0,0 +1,18 @@
+package com.fororoms.gateway.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    @ResponseBody
+    public String handleError() {
+        //TODO: Puedes devolver una vista personalizada para el error aquí
+        return "Página no encontrada";
+    }
+
+}
